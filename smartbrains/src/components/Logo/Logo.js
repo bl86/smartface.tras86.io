1bdb83332886bf6f1a258c322481291edcf1c52a
tras86 <andrejb86@gmail.com>
2018-05-18 17:42:53 +0200

    Skeleton

---
+++ smartbrains/src/components/Logo/Logo.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Tilt from 'react-tilt';
+import brain from './brain.png';
+import './Logo.css';
+
+const Logo = () => {
+  return (
+    <div className='ma4 mt0'>
+      <Tilt className="Tilt br2 shadow-2" options={{ max : 25 }} style={{ height: 130, width: 130 }} >
+       <div className="Tilt-inner pa2"><img alt='logo'  src={brain} /></div>
+      </Tilt>
+    </div>
+  );
+}
+
+export default Logo;
