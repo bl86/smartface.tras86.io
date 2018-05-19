30f11286fbef8d163c3f6105be743321f6c8b50f
tras86 <andrejb86@gmail.com>
2018-05-19 12:00:02 +0200

    REcognition done.

---
+++ smartbrains/src/components/Rank/Rank.js
@@ -0,0 +1,17 @@
+import React from 'react';
+//import './Rank.css'
+
+const Rank = () => {
+  return (
+    <div>
+      <div className='white f3'>
+        {'Text for rank'}
+      </div>
+      <div className='white f1'>
+        {'Number 5'}
+      </div>
+    </div>
+  );
+}
+
+export default Rank;
